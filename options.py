64d16b1c647421c02009609781a66a65c69d4ab5
HvE_Infra <hugo.vanegeraat@infranea.com>
2023-08-23 23:04:37 +0200

    Commit function.py and option.py

--- options.py
+++ options.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#This is a code which arranges the pandas view settings
 def set_options_for_pandas():
     pd.set_option("display.max_columns", 10)
     pd.set_option('display.max_colwidth', 400)
